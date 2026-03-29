76e5e0c799bd2c40da598bcd89282fc8be6f4a9c
Riddhiman Raguraman <76609486+RiddhimanRaguraman@users.noreply.github.com>
2026-03-29 06:00:42 -0500

    new class setup for the create button

---
+++ Neelam/NeelamEditor/GameProject/NewProject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NeelamEditor.GameProject
+{
+    class NewProject : ViewModelBase
+    {
+        private string _name = "NewProject";
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        private string _path = "NewProject";
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                if (_path != value)
+                {
+                    _path = value;
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
+    }
+
+}
