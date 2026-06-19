35bf905b020a12de4dbcd663761d77553716b671
Riddhiman Raguraman <76609486+RiddhimanRaguraman@users.noreply.github.com>
2026-06-18 20:12:46 -0500

    added templates and the namespace rework

--- Neelam/NeelamEditor/GameProject/CreateProject/NewProject.cs
+++ Neelam/NeelamEditor/GameProject/CreateProject/NewProject.cs
@@ -151,8 +151,13 @@ namespace NeelamEditor.GameProject
                 dirinfo.Attributes |= FileAttributes.Hidden;
                 File.Copy(template.IconFilePath, Path.GetFullPath(Path.Combine(dirinfo.FullName, "Icon.png")));
                 File.Copy(template.ScreenshotFilePath, Path.GetFullPath(Path.Combine(dirinfo.FullName, "Screenshot.png")));
-                var project = new Project(ProjectName, path);
-                Serializer.ToFile(project, path + "ProjectName" + Project.Extension);
+
+                var projectXml = File.ReadAllText(template.ProjectFilePath);
+                projectXml = string.Format(projectXml, ProjectName, ProjectPath);
+                var projectPath = Path.GetFullPath(Path.Combine(path + $"{ProjectName}{Project.Extension}"));
+                File.WriteAllText(projectPath, projectXml);
+
+
                 return path; 
             }
             catch (Exception ex)
---
+++ Neelam/NeelamEditor/GameProject/OpenProject/OpenProject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeelamEditor.GameProject
+{
+    class OpenProject : ViewModelBase
+    {
+
+    }
+}
